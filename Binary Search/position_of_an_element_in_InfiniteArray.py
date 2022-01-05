c8edf624b7af4b17e6ae1c83d962f61972f73765
Raj Jada <Rajjada001@gmail.com>
2022-01-05 17:05:24 +0530

    InfiniteSortedArray

--- Binary Search/position_of_an_element_in_InfiniteArray.py
+++ Binary Search/position_of_an_element_in_InfiniteArray.py
@@ -5,7 +5,7 @@ start = time.time()
 def InFiniteArray(a,t):
     s,e = 0,1
     # condition for the target to lie in the range
-    while(a[e] < t):
+    while(t > a[e]):
         newStart = e + 1
         # double the end
         # previous end + size of the box
@@ -14,7 +14,7 @@ def InFiniteArray(a,t):
     return BinarySearch(a,t,s,e)
 
 def BinarySearch(a,t, l,h):
-    while(l<h):
+    while(l<=h):
         mid = l+(h-l)//2
 
         if(a[mid]>t):
@@ -23,11 +23,12 @@ def BinarySearch(a,t, l,h):
             l = mid+1
         else:
             return mid
+    return -1
     
 
 t = int(input("Enter T:"))
 for _ in range(t):
-    t = int(input("Enter n:"))
+    n = int(input("Enter n:"))
     a = list(map(int, input().rstrip().split()))
-    print(floor(a,t))
+    print(InFiniteArray(a,n))
     print("It took %s milli seconds"%((time.time()-start)*100))
