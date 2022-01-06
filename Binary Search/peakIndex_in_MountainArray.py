ce15b5124cb107c00e143ffe6beb906799ff222e
Raj Jada <Rajjada001@gmail.com>
2022-01-06 16:02:46 +0530

    LCM and GCD

---
+++ Binary Search/peakIndex_in_MountainArray.py
@@ -0,0 +1,22 @@
+# Using binary search to find the square root of a number
+import time
+start = time.time()
+
+
+def peakIndexMountainArray(a):
+    l,h=0,len(a)-1
+    while(l<h):
+        mid = l+(h-l)//2
+        if(a[mid]>a[mid+1]):
+            h = mid
+        else:
+            l = mid+1
+    return l
+
+
+print(peakIndexMountainArray([1,2,4,5,3,2,1])) # index:3
+print(peakIndexMountainArray([12,51,42,7])) #index:1
+print(peakIndexMountainArray([1,2,65,87,98,118,97,85,63,1])) #index:5
+
+
+print("It took %s milli seconds"%((time.time()-start)*1000))
