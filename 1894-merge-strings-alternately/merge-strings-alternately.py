2c9a37a487d0e36e340ec50cfb45737375094f89
Raj Jada <Rajjada001@gmail.com>
2024-02-19 19:19:22 -0500

    Time: 37 ms (51.90%) | Memory: 16.7 MB (52.90%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,18 +1,12 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        # brute force
-        small, large = '',''
-        if len(word1)>len(word2):
-            small=word2
-            l = word1
-        else:
-            small=word1
-            l = word2
+        
         res = []
-        for i in range(len(small)):
-            res.append(word1[i])
-            res.append(word2[i])
-        large = l[len(small):]
-        for ch in large:
-            res.append(ch)
+        i=0
+        while i<len(word1) or i<len(word2):
+            if i<len(word1):
+                res.append(word1[i])
+            if i<len(word2):
+                res.append(word2[i])
+            i+=1
         return ''.join(ch for ch in res)
